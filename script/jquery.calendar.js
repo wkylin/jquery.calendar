bd854f8cac06fc60fbd2bc205ff6432f2f9b9a0b
wangjingang <wangjingang@chexiang.com>
2015-06-17 11:24:56 +0800

    jquery.calendar.js init

--- script/jquery.calendar.js
+++ script/jquery.calendar.js
@@ -1,7 +1,3 @@
-/**
- * Created by wangjingang on 2015/6/17.
- */
-
 ;
 (function (factory) {
     if (typeof define === 'function' && define.amd) {
@@ -13,32 +9,69 @@
     else {
         factory(jQuery);
     }
-}
-(function ($) {
+}(function ($) {
 
     var pluginName = "wkCalendar",
         defaults = {
-            
+            todayDate: new Date()
         };
 
     function Plugin($container, options) {
         this.options = $.extend({}, defaults, options);
         var self = this;
-        var $viewport = $container.find(".ui-dropdown-menu-inner");
+        var oDate = this.options.todayDate;
+        var dayNum = 0;
+        var oYear = oDate.getFullYear();
+        var oMonth = oDate.getMonth() + 1;
+        var oDay = oDate.getDate();
+
+        var currentYear = oDate.getFullYear();
+        var currentMonth = oDate.getMonth() + 1;
+        var currentDay = oDate.getDate();
+
+        //判断月份的天数
+        if (oMonth == 1 || oMonth == 3 || oMonth == 5 || oMonth == 7 || oMonth == 8 || oMonth == 10 || oMonth == 12) {
+            dayNum = 31;
+        }
+        else if (oMonth == 4 || oMonth == 6 || oMonth == 9 || oMonth == 11) {
+            dayNum = 30;
+        }
+        else if (oMonth == 2 && _isLeapYear(oYear)) {
+            dayNum = 29;
+        }
+        else {
+            dayNum = 28;
+        }
 
         function _initialize() {
 
             self.update();
-            _init();
+            
             return self;
         }
 
         this.update= function(){
-            console.log("update");
-        }
+            _init();
+        };
         
+        //初始化
         function _init(){
-            console.log($viewport.html());
+            
+            console.log(dayNum);
+        }
+
+        //判断是否润年  
+        function _isLeapYear(year) {
+            if (year % 4 == 0 && year % 100 != 0) {
+                return true;
+            }
+            else {
+                if (year % 400 == 0) {
+                    return true;
+                }else {
+                    return false;
+                }
+            }
         }
         
         return _initialize();
