9437f328e35437c5e7c59eaab020204bb2c0350b
wangjingang <wangjingang@chexiang.com>
2015-06-17 21:23:48 +0800

    show calendar

--- script/jquery.calendar.js
+++ script/jquery.calendar.js
@@ -43,21 +43,30 @@
             dayNum = 28;
         }
 
+
+        //生成表格 tbody
+
+        var $table = $(".ui-calendar-table");
+        var $tBody = $table.find("tbody");
+        var $year = $(".ui-calendar-year-box .ui-dropdown-btn");
+        var $month = $(".ui-calendar-month-box .ui-dropdown-btn");
+
+
         function _initialize() {
 
             self.update();
-            
+
             return self;
         }
 
-        this.update= function(){
+        this.update = function () {
             _init();
         };
-        
+
         //初始化
-        function _init(){
-            
-            console.log(dayNum);
+        function _init() {
+            _renderTbody();
+            _showDate(oDate.getFullYear(), oDate.getMonth());
         }
 
         //判断是否润年  
@@ -68,12 +77,95 @@
             else {
                 if (year % 400 == 0) {
                     return true;
-                }else {
+                } else {
                     return false;
                 }
             }
         }
-        
+
+        function _renderTbody() {
+            for (var j = 0; j < 6; j++) {
+                var oTr = document.createElement('tr');
+                for (var k = 0; k < 7; k++) {
+                    var oTd = document.createElement('td');
+                    $(oTr).append(oTd);
+                }
+                $($tBody).append(oTr);
+            }
+            $table.append($tBody);
+        }
+
+        function _showDate(year, month) {
+
+            $year.text(oYear);
+            $month.text(oMonth);
+
+            //设置当月第一天的星期数
+            var aTd = $table.find('td');
+            $(aTd).html('');
+            //$(aTd).removeClass("current");
+            oDate.setFullYear(year);
+            oDate.setMonth(month);
+            oDate.setDate(1);
+
+            switch (oDate.getDay()) {
+                case 0:
+                    for (var i = 0; i < dayNum; i++) {
+                        $(aTd).eq(i + 6).html(_showTd(i));
+                    }
+                    break;
+                case 1:
+                    for (var i = 0; i < dayNum; i++) {
+                        $(aTd).eq(i).html(_showTd(i));
+                    }
+                    break;
+                case 2:
+                    for (var i = 0; i < dayNum; i++) {
+                        $(aTd).eq(i + 1).html(_showTd(i));
+                    }
+                    break;
+                case 3:
+                    for (var i = 0; i < dayNum; i++) {
+                        $(aTd).eq(i + 2).html(_showTd(i));
+                    }
+                    break;
+                case 4:
+                    for (var i = 0; i < dayNum; i++) {
+                        $(aTd).eq(i + 3).html(_showTd(i));
+                    }
+                    break;
+                case 5:
+                    for (var i = 0; i < dayNum; i++) {
+                        $(aTd).eq(i + 4).html(_showTd(i));
+                    }
+                    break;
+                case 6:
+                    for (var i = 0; i < dayNum; i++) {
+                        $(aTd).eq(i + 5).html(_showTd(i));
+                    }
+                    break;
+            }
+        }
+
+
+        function _showTd(key){
+            var dateStr = oYear + '-' + _plusZero(oMonth) + '-' + _plusZero(1 + key);
+            var weekend = new Date(dateStr);
+            var tdHtml = '<div class="ui-calendar-relative"><a data-date="' + dateStr + '" class="';
+            if (key == currentDay - 1) {
+                tdHtml += 'ui-calendar-table-today';
+            }
+            if (weekend.getDay() == 0 || weekend.getDay() == 6) {
+                tdHtml += ' ui-calendar-table-weekend';
+            }
+            tdHtml += '" href="javascript:void(0);"><span class="ui-calendar-daynumber">' + (key + 1) + '</span></a></div>';
+            return tdHtml;
+        }
+        function _plusZero(str) {
+            return str < 10 ? '0' + str : str
+        }
+
+        //初始化方法
         return _initialize();
     }
 
